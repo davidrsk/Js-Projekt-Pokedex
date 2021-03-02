cb75a71f9b70c91796269a10b418d56a65cd16ec
David Rask <david.rask@hotmail.com>
2021-03-02 21:24:11 +0100

    äntligen nåt gif'arna från api'n

--- js/index.js
+++ js/index.js
@@ -1,6 +1,37 @@
 "use strict";
 // https://pokeapi.co/
 
-let outputBox = document.getElementById("output-box");
+//TODO
+// hämta http och lagra url i variabel
+// fixa så att input-lådan påverkar pokémon sprite
+// 
 
-const url = new URL ("https://pokeapi.co/api/v2/pokemon/");
+let outputBox = document.getElementById("output-box");
+let inputBox = document.getElementById("pokemon-index")
+let sprite = document.getElementById("sprite");
+
+inputBox.oninput = function(){
+    updateSprite(inputBox.value);
+}
+
+function updateSprite(idIndex){
+    const url = new URL ("https://pokeapi.co/api/v2/pokemon/" + idIndex);
+
+    const serverResponse = fetch(url); //hämtar url, stoppas i serverResponse
+
+ serverResponse.then(response=>response.json()).then(data=>{ //
+    sprite.src = data.sprites.versions["generation-v"]["black-white"].animated.front_default;
+    console.log(data)}); 
+}
+
+//sprites.versions.generation-v.black-white.animated.front_default;
+    //serverResponse är en promise
+// .then är när vi fått datan vi bett om, lagras i response. => function(response) { return response.json()}
+
+
+
+/*let xhr = new XMLHttpRequest(); //skapar objektet
+
+xhr.open("GET", url);
+
+xhr.responseType = "json";*/
